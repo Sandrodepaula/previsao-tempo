ae0931a82e2389dff685e4272cef5dfc9a388c1b
unknown <sandroads@yahoo.com>
2024-01-25 22:29:45 -0300

    Exclusão do icone da bandeira

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Váriaveis e seleção de elementos
 const apiKey = "f91529f7f5cf35b6524901f2d75a5d71";
-const apiCountryURL = "https://countryflagsapi.com/png/";
+
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -31,7 +31,6 @@ const showWeatherData = async (city) => {
     tempElement.innerText = parseInt(data.main.temp);
     descElement.innerText = data.weather[0].description;
     weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
     humidityElement.innerText = `${data.main.humidity}%`;
     windElement.innerText = `${data.wind.speed}km/h`;
 
